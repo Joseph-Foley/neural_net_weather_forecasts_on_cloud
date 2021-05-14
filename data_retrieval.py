1ed0d014d875dc687357ccc7f4bddc3253ca6260
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2021-05-14 18:56:46 +0100

    success in retrieving json from API

--- data_retrieval.py
+++ data_retrieval.py
@@ -16,3 +16,18 @@ import requests
 # =============================================================================
 # PROCESS
 # =============================================================================
+#load API key
+with open('api_key.txt') as f:
+    api_key = f.read()
+    
+#url
+url =\
+"""
+https://weather.visualcrossing.com/VisualCrossingWebServices/rest/
+services/timeline/London%2C%20ENG%2C%20GB/2021-5-12/2021-5-13?
+unitGroup=uk&key=[API_KEY]&include=obs
+""".replace('\n', '')\
+   .replace('[API_KEY]', api_key)
+
+#make request
+req = requests.get(url)
