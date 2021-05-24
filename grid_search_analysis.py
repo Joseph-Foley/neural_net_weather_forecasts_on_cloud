c52ed8f8148884b4f1ce80e561dbd5ca07679382
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2021-05-24 19:28:16 +0100

    analysed smooth

--- grid_search_analysis.py
+++ grid_search_analysis.py
@@ -10,6 +10,10 @@ import numpy as np
 import datetime as dt
 import matplotlib.pyplot as plt
 
+# =============================================================================
+# NO SMOOTH
+# =============================================================================
+
 gs = pd.read_csv('Data/results_table_temp.csv')
 
 gs.groupby('length')['val_mae'].mean().plot(kind='bar', title='mean'); plt.show()
@@ -30,4 +34,30 @@ The model is learning to predict the future better by being fed more history at 
 Dropout does not improve the model performance. Its not often dropout is used in RNN networks, they tend to overfit for other reasons.
 LSTM is just slightly better than GRU.
 Increasing the number of RNN layers does not improve the predictive power of the model.
+"""
+
+# =============================================================================
+# SMOOTH
+# =============================================================================
+gs = pd.read_csv('Data/results_table_temp_smooth.csv')
+
+gs.groupby('length')['val_mae_og'].mean().plot(kind='bar', title='mean'); plt.show()
+gs.groupby('layers_num')['val_mae_og'].mean().plot(kind='bar', title='mean'); plt.show()
+gs.groupby('layers_type')['val_mae_og'].mean().plot(kind='bar', title='mean'); plt.show()
+gs.groupby('units')['val_mae_og'].mean().plot(kind='bar', title='mean'); plt.show()
+gs.groupby('g_filt')['val_mae_og'].mean().plot(kind='bar', title='mean'); plt.show()
+
+gs.groupby('length')['val_mae_og'].mean().plot(kind='bar', title='min'); plt.show()
+gs.groupby('layers_num')['val_mae_og'].mean().plot(kind='bar', title='min'); plt.show()
+gs.groupby('layers_type')['val_mae_og'].mean().plot(kind='bar', title='min'); plt.show()
+gs.groupby('units')['val_mae_og'].mean().plot(kind='bar', title='min'); plt.show()
+gs.groupby('g_filt')['val_mae_og'].mean().plot(kind='bar', title='min'); plt.show()
+
+print('Best Parameters: {}'.format(gs.sort_values('val_mae_og').iloc[0]))
+
+"""
+Performance is mostly influenced by our smoothing factor.
+The other hyperparameters are of little consequence it seems.
+Performance decreases the more we smooth. The model fits onto something that
+is too far removed from the real series. the signal is altered too muich.
 """
