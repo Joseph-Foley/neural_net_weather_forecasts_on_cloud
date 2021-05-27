e1f739d86cf69594965562056398e2c0cae86406
DESKTOP-AM4-JF <joseph_foley@hotmail.com>
2021-05-27 20:04:41 +0100

    succesfully made dash board that plots live updates from the API

--- Plotly/hourly_live_updates.py
+++ Plotly/hourly_live_updates.py
@@ -6,6 +6,11 @@ import os
 import pandas as pd
 import requests
 
+import plotly.graph_objs as go
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
 def getSeries():
     #load API key
@@ -23,6 +28,58 @@ def getSeries():
     
     #convert to pandas dataframe
     hours = pd.DataFrame(req_json['days'][0]['hours'])
-
+    
+    #drop where no temp
+    hours = hours[hours['temp'].notna()]
+    
     return hours
 
+def plotlyData(hours):
+    trace = go.Scatter( x = hours['datetime'],
+                        y = hours['temp'],
+                        mode = 'lines+markers',
+                        name = 'temp_hist')
+    
+    return trace
+
+#layout = go.Layout(title = 'Temperature hourly (live)')
+
+
+#DASH
+app = dash.Dash()
+
+colors = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
+
+app.layout = html.Div(children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    dcc.Graph(id='live-update-graph',
+              style={'backgroundColor': colors['background']}
+            ),
+    dcc.Interval(
+        id='interval-component',
+        interval=1*60*60*1000, # 1hr * 60mins * 60secs * 1000milisecs
+        n_intervals=0
+    )])
+
+@app.callback(Output('live-update-graph','figure'),
+              [Input('interval-component', 'n_intervals')])
+def update_graph(n):
+    hours = getSeries()
+    
+    fig = go.Figure(
+        data = [plotlyData(hours)])
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server()
